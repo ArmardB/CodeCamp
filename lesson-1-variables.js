ddd2728db390179d470fa146103ea42baaed48de
Armard Bellamy <armardbellamy@gmail.com>
2019-01-31 22:40:19 -0500

    Added lesson one review

---
+++ lesson-1-variables.js
@@ -0,0 +1,23 @@
+// Watch this lesson: https://vimeo.com/163153277
+
+/**
+  Sample: Make the phrase 'Hello World' appear on the console
+*/
+console.log('Hello World');
+
+/**
+ Problem 1: Declare a variable named "myDreamCar" and set it equal to the name of your favorite car
+ */
+
+ /** 
+  Problem 2: Declare a variable and set it equal to the number 2000. Declare a second variable and set it equal to the number 43. Declare a third variable (name it anything you want) and set it equal to the result of adding the first and second numbers together. 
+ */
+
+ /**
+  Problem 3: Set the value of a variable named "mutable" to the String value of 'This is my value'. Console log this value. Take the same variable named "mutable" and set it to a new value of "My value changed." If you console log out the value of mutable, what will it read?
+ */
+
+ /**
+  Assign the value of PI (3.14) to a variable named 'pi'. Assign the value of 4 to a variable named 'square'. Create a variable named 'result'. Multiply 'pi' times 'result' then divide by 2. Console log your the value of result.
+ */
+
